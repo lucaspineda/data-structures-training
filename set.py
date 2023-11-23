abeaabe5e4e618c0441e92d8317391fde6e8a0bb
lucaspineda <lucas.pineda@hotmail.com>
2023-11-23 01:07:54 -0300

    solve set method

--- set.py
+++ set.py
@@ -74,6 +74,13 @@ class LinkedList:
             temp = temp.next
         return temp
         
+    def set_value(self, index, value):
+        temp = self.get(index)
+        if temp:
+            temp.value = value
+            return True
+        return False
+
     ## WRITE SET_VALUE METHOD HERE ##
     #                               #
     #                               #
