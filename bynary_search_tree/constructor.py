1e1d6f92282ce43512c2f3552b7003511d5bd9ed
lucaspineda <lucas.pineda@hotmail.com>
2023-12-24 20:04:43 -0300

    constructor for binary tree

---
+++ bynary_search_tree/constructor.py
@@ -0,0 +1,38 @@
+class Node:
+    def __init__(self, value) -> None:
+        self.value = value
+        self.left = None
+        self.right = None
+    ## WRITE NODE CONSTRUCTOR HERE ##
+    #                               #
+    #                               #
+    #                               #
+    #                               #
+    #################################
+        
+
+class BinarySearchTree:
+    def __init__(self) -> None:
+        self.root = None
+    ## WRITE BST CONSTRUCTOR HERE ##
+    #                              #
+    #                              #
+    #                              #
+    #                              #
+    ################################
+
+
+
+
+my_tree = BinarySearchTree()
+
+print("Root:", my_tree.root)
+
+
+ 
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    Root: None
+
+"""
