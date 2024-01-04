59e72c269d55fc49760e35ee4a7df41bf1f26b0b
lucaspineda <lucas.pineda@hotmail.com>
2024-01-04 01:21:56 -0300

    solution for two sum

--- hash_table_leet_code/two_sum.py
+++ hash_table_leet_code/two_sum.py
@@ -6,7 +6,25 @@
 ###############################
     
     
-    
+def two_sum(num_array, target):
+    numbers_dict = {}
+    first_number_index = None
+    second_number_index = None
+    i = 0
+    for number in num_array:
+        numbers_dict[number] = i
+        i += 1
+    for index, number_item in enumerate(numbers_dict):
+        second_number = target - number_item
+        if second_number != number_item:
+            if second_number in numbers_dict:
+                first_number_index = index
+                second_number_index = numbers_dict.get(second_number)
+                break
+    if first_number_index is not None:
+        return [first_number_index, second_number_index]
+    else:
+        return []
     
 print(two_sum([5, 1, 7, 2, 9, 3], 10))  
 print(two_sum([4, 2, 11, 7, 6, 3], 9))  
@@ -15,7 +33,9 @@ print(two_sum([1, 3, 5, 7, 9], 10))
 print ( two_sum([1, 2, 3, 4, 5], 10) )
 print ( two_sum([1, 2, 3, 4, 5], 7) )
 print ( two_sum([1, 2, 3, 4, 5], 3) )
+
 print ( two_sum([], 0) )
+print ( two_sum([1, 2, 2, 4, 5], 3) )
 
 
 
