bd28bc64eff56e192a52e2fb4290391403852e93
lucaspineda <lucas.pineda@hotmail.com>
2024-01-08 11:41:12 -0300

    solution for unique char

--- hash_table_leet_code/has_unique_chars.py
+++ hash_table_leet_code/has_unique_chars.py
@@ -6,6 +6,13 @@
 ########################################
 
 
+def has_unique_chars(string):
+    char_set = set()
+    for char in string:
+        if char in char_set:
+            return False
+        char_set.add(char)
+    return True
 
 
 print(has_unique_chars('abcdefg')) # should return True
