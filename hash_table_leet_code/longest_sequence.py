9f4684aaf6a34395a676d7084b6e7a5c68d959a2
lucaspineda <lucas.pineda@hotmail.com>
2024-01-09 08:31:57 -0300

    create question

---
+++ hash_table_leet_code/longest_sequence.py
@@ -0,0 +1,19 @@
+# WRITE LONGEST_CONSECUTIVE_SEQUENCE FUNCTION HERE #
+#                                                  #
+#                                                  #
+#                                                  #
+#                                                  #
+####################################################
+
+
+
+print( longest_consecutive_sequence([100, 4, 200, 1, 3, 2]) )
+
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    4
+
+"""
