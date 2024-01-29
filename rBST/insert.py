04b456165c93dcef22e483770d7ea765d5744769
lucaspineda <lucas.pineda@hotmail.com>
2024-01-29 05:02:07 -0300

    implement insert

--- rBST/insert.py
+++ rBST/insert.py
@@ -9,6 +9,24 @@ class BinarySearchTree:
     def __init__(self):
         self.root = None
 
+    def __r_insert(self, current_node, value):
+        if current_node is None:
+            return Node(value)
+        
+        if value < current_node.value:
+            current_node.left = self.__r_insert(current_node.left, value)
+        
+        if value > current_node.value:
+            current_node.right = self.__r_insert(current_node.right, value)
+
+        return current_node
+        
+    def r_insert(self, value):
+        if self.root is None:
+            self.root = Node(value)
+            return self.root
+        return self.__r_insert(self.root, value)
+        
     ## WRITE R_INSERT METHODS HERE ##
     #                               #
     #                               #
