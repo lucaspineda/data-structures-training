d7c01c3f9b3b3e545b6f29d9524a4e3a509208aa
lucaspineda <lucas.pineda@hotmail.com>
2024-02-07 05:42:30 -0300

    bubble sort enhancements

--- sorts/bubble_sort.py
+++ sorts/bubble_sort.py
@@ -8,8 +8,8 @@
 
 
 def bubble_sort(my_list):
-    for i in range(len(my_list) - 1):
-        for j in range(len(my_list) - 1):
+    for i in range(len(my_list) - 1, 0, -1):
+        for j in range(i):
             # print(my_list[j], my_list[j + 1])
             if my_list[j] > my_list[j + 1]:
                 higher_value = my_list[j]
