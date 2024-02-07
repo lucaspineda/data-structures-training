fe995bcae4eb82dd8c0c97d76f64c844447d08cd
lucaspineda <lucas.pineda@hotmail.com>
2024-02-07 05:31:50 -0300

    bubble sort

---
+++ sorts/bubble_sort.py
@@ -0,0 +1,30 @@
+## WRITE BUBBLE_SORT FUNCTION HERE ##
+#                                   #
+#                                   #
+#                                   #
+#                                   #
+##################################### 
+
+
+
+def bubble_sort(my_list):
+    for i in range(len(my_list) - 1):
+        for j in range(len(my_list) - 1):
+            # print(my_list[j], my_list[j + 1])
+            if my_list[j] > my_list[j + 1]:
+                higher_value = my_list[j]
+                my_list[j] = my_list[j + 1]
+                my_list[j + 1] = higher_value
+    return my_list
+
+print(bubble_sort([4,2,6,1]))
+# print(bubble_sort([4,2,6,5,1,3]))
+
+ 
+ 
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    [1, 2, 3, 4, 5, 6]
+    
+ """
