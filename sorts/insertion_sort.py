e77c50e259a5923cd1071f0b0e70ea9e7d804ac4
lucaspineda <lucas.pineda@hotmail.com>
2024-02-09 17:05:30 -0300

    solution for insert sort

---
+++ sorts/insertion_sort.py
@@ -0,0 +1,34 @@
+## WRITE INSERTION_SORT FUNCTION HERE ##
+#                                      #
+#                                      #
+#                                      #
+#                                      #
+######################################## 
+
+
+def insertion_sort(my_list):
+    for i in range(1, len(my_list)):
+        temp = my_list[i]
+        j = i - 1
+
+        while temp < my_list[j]:
+            my_list[j+1] = my_list[j]
+            my_list[j] = temp
+            j -= 1
+            if j == -1:
+                break
+
+    return my_list
+
+
+print(insertion_sort([4,2,6,5,1, 3]))
+
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    [1, 2, 3, 4, 5, 6]
+    
+ """
+
