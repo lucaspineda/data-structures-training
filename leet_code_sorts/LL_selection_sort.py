f4efc392e0142ff18fa9fb187384c226413d8e77
lucaspineda <lucas.pineda@hotmail.com>
2024-02-10 00:00:46 -0300

    solution

---
+++ leet_code_sorts/LL_selection_sort.py
@@ -0,0 +1,92 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+        
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+
+    def selection_sort(self):
+        if self.length < 2:
+            return
+        current = self.head
+        while current.next:
+            min_node = current
+            inner_current = current.next
+            while inner_current:
+                if inner_current.value < min_node.value:
+                    min_node = inner_current
+                inner_current = inner_current.next
+            if min_node != current:
+                min_node.value, current.value = current.value, min_node.value
+            current = current.next
+                
+
+    # WRITE SELECTION_SORT METHOD HERE #
+    #                                  #
+    #                                  #
+    #                                  #
+    #                                  #
+    ####################################
+
+
+
+my_linked_list = LinkedList(4)
+my_linked_list.append(2)
+my_linked_list.append(6)
+my_linked_list.append(5)
+my_linked_list.append(1)
+my_linked_list.append(3)
+
+print("Linked List Before Sort:")
+my_linked_list.print_list()
+
+my_linked_list.selection_sort()
+
+print("\nSorted Linked List:")
+my_linked_list.print_list()
+
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    Linked List Before Sort:
+    4
+    2
+    6
+    5
+    1
+    3
+
+    Sorted Linked List:
+    1
+    2
+    3
+    4
+    5
+    6
+
+"""
+
