29191ce2e60dd16b464f25c1cb445f79c6cc64ce
lucaspineda <lucas.pineda@hotmail.com>
2024-02-13 00:45:33 -0300

    implement

--- leet_code_sorts/LL_insertion_sort.py
+++ leet_code_sorts/LL_insertion_sort.py
@@ -28,13 +28,40 @@ class LinkedList:
         self.length += 1
 
     def insertion_sort(self):
-        if self.length > 2:
+        if self.length < 2:
             return
-        # sorted_until = self.head
-        # while sorted_until.next:
-        #     unsorted = sorted_until.next
-            # while sorted_until < :
-            #     if 
+        # unsorted_from = self.head.next
+        # while unsorted_from:
+        #     sorted_until = self.head
+        #     while unsorted_from.value < sorted_until.value:
+        #         print('chamouu')
+        #         temp = sorted_until.value
+        #         sorted_until.value = unsorted_from.value
+        #         unsorted_from.value = temp
+        #         sorted_until = sorted_until.next
+        #     unsorted_from = unsorted_from.next
+
+
+        sorted_list_head = self.head
+        unsorted_list_head = self.head.next
+        sorted_list_head.next = None
+        while unsorted_list_head is not None:
+            current = unsorted_list_head
+            unsorted_list_head = unsorted_list_head.next
+            if current.value < sorted_list_head.value:
+                current.next = sorted_list_head
+                sorted_list_head = current
+            else:
+                search_pointer = sorted_list_head
+                while search_pointer.next is not None and current.value > search_pointer.next.value:
+                    search_pointer = search_pointer.next
+                current.next = search_pointer.next
+                search_pointer.next = current
+        self.head = sorted_list_head
+        temp = self.head
+        while temp.next is not None:
+            temp = temp.next
+        self.tail = temp
 
     # WRITE INSERTION_SORT METHOD HERE #
     #                                  #
