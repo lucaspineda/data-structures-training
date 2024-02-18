56fbfa73b10d51449a1f7c52d52969b931185eba
lucaspineda <lucas.pineda@hotmail.com>
2024-02-18 02:18:39 -0300

    create question

---
+++ merge_sort/merge_two_LL.py
@@ -0,0 +1,81 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+        
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+
+    # WRITE MERGE METHOD HERE #
+    #                         #
+    #                         #
+    #                         #
+    #                         #
+    ###########################
+    def merge(self, other_list):
+        dummy = Node(0)
+        current = dummy
+        node1 = self.head
+        node2 = other_list.head
+        while current:
+            if (node1.value < node2.value):
+                current = node1
+                node1 = node1.next
+            else:
+                current.next = node2
+                node2 = node2.next
+
+            
+    
+
+
+l1 = LinkedList(1)
+l1.append(3)
+l1.append(5)
+l1.append(7)
+
+
+l2 = LinkedList(2)
+l2.append(4)
+l2.append(6)
+l2.append(8)
+
+l1.merge(l2)
+
+l1.print_list()
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    1 
+    2 
+    3 
+    4 
+    5 
+    6 
+    7
+    8
+
+"""
