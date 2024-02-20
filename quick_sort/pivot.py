61035837517fb60e9fa16a511fe24c35552c72f5
lucaspineda <lucas.pineda@hotmail.com>
2024-02-20 00:17:58 -0300

    pivot solution

--- quick_sort/pivot.py
+++ quick_sort/pivot.py
@@ -12,14 +12,21 @@ def swap(my_list, index1, index2):
 ############################### 
 
 
-
+def pivot(my_list, pivot_index, end_index):
+    swap_index = pivot_index
+    for i in range(pivot_index + 1, end_index + 1):
+        if my_list[i] < my_list[pivot_index]:
+            swap_index += 1
+            swap(my_list, swap_index, i)
+    swap(my_list, pivot_index, swap_index)
+    return swap_index
 
 my_list = [4,6,1,7,3,2,5]
 
 print('List before running pivot():')
 print(my_list)
 
-returned_pivot_index = pivot(my_list, 0, 6)
+returned_pivot_index = pivot(my_list, 6, 6)
 
 print('\nList after running pivot():')
 print(my_list)
