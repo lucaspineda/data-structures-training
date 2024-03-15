c00f853899035e00eaad7071a6b355320afb3d27
lucaspineda <lucas.pineda@hotmail.com>
2024-03-15 01:55:29 -0300

    finish project

--- flask/example_app.py
+++ flask/example_app.py
@@ -1,9 +1,16 @@
-from flask import Flask, render_template
+import requests
+from flask import Flask, render_template, jsonify
+from meme import Meme
+import json
 
 app = Flask(__name__)
 
 @app.route("/")
 def return_meme():
-    return render_template('index.html')
+    meme_content = json.loads(requests.get('https://meme-api.com/gimme').content)
+    new_meme = Meme(meme_content.get('url'), meme_content.get('title'))
+    meme_dict = {'url': new_meme.img_source, 'title': new_meme.title}  # Create a dictionary
+
+    return meme_dict
 
 app.run("0.0.0.0", port=81)
---
+++ flask/meme.py
@@ -0,0 +1,4 @@
+class Meme:
+    def __init__(self, img_source, title):
+        self.img_source = img_source
+        self.title = title
