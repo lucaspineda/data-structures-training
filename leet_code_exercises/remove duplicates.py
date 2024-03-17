0b6a6312dffd642fbf76a0607b3b332a9e36faa9
lucaspineda <lucas.pineda@hotmail.com>
2024-03-17 17:23:49 -0300

    solution for remove duplicates

--- leet_code_exercises/remove duplicates.py
+++ leet_code_exercises/remove duplicates.py
@@ -1,3 +1,22 @@
 class Solution:
     def removeDuplicates(self, nums: list[int]) -> int:
-        
+        i = 1
+        current = nums[0]
+        counter = 0
+        nums_len = len(nums)
+        while i < len(nums):
+            if nums[i] == current:
+                nums.pop(i)
+                counter += 1
+                continue
+            current = nums[i]
+            i += 1
+        return nums_len - counter
+
+
+solution = Solution()  # Instantiate the Solution class
+nums = [1,2,2,3]
+
+
+print(solution.removeDuplicates(nums))  # Call the merge function
+print(nums)
